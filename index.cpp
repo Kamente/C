c64ae7d48c35b8efb6dc05e8aa8288e2af561efe
Kamente <justinm48000@gmail.com>
2023-07-15 12:38:07 +0300

    initial commit

---
+++ index.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1 = 8;
+    int num2 = 12;
+    int num3 = 3;
+    int max = num1;
+
+    if (num2 > max)
+    {
+        max = num2;
+    }
+    if (num3 > max)
+    {
+        max = num3;
+    }
+    cout << max << endl;
+
+    return 0;
+}
